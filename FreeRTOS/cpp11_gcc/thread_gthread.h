21486ddfe762ff9fbce011680898f0996c64e75c
Pablo Ghiglino <pablo.ghiglino@klepsydra.com>
2021-03-18 21:02:09 -0700

    WIP

--- FreeRTOS/cpp11_gcc/thread_gthread.h
+++ FreeRTOS/cpp11_gcc/thread_gthread.h
@@ -118,7 +118,7 @@ public:
     {
       critical_section critical;
 
-      xTaskCreate(foo, "Task", 512, this, tskIDLE_PRIORITY + 1, &_taskHandle);
+      xTaskCreate(foo, "Task", 2048, this, tskIDLE_PRIORITY + 1, &_taskHandle);
       if (!_taskHandle)
         std::terminate();
 
